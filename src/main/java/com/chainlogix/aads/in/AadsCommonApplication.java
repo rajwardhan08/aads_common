fbdb9e45eeb931980052316a6a37d00d4b69d7d3
Anurag Kumar <anurag@AADS.LOCAL>
2022-06-22 12:44:57 +0530

    added new file

---
+++ src/main/java/com/chainlogix/aads/in/AadsCommonApplication.java
@@ -0,0 +1,13 @@
+package com.chainlogix.aads.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadsCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AadsCommonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chainlogix/aads/in/AadsCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chainlogix.aads.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadsCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
